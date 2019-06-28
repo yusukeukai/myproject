c9f671334bdbcf35d12a9686ab637426ef9a5016
aimarl21 <aimarl21-justin21-yu221@outlook.jp>
2019-06-28 22:39:23 +0900

    Lesson6プロジェクト

---
+++ Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * カメラのｚ位置とtransform(自分自身)のz位置を比較して、もしカメラの位置よりtransformが後ろに行ったらDestroy(自分自身);
+         */
+        if (transform.localPosition.z < Camera.main.transform.localPosition.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
